05156dc532dd406ae54ee6b13f3f26c48feb8848
aliifam <aliif.arief.m@gmail.com>
2022-12-22 23:46:29 +0700

    feat: adapter & recyclerview

--- app/src/main/java/com/aliif/lpmuas/activty/MainActivity.java
+++ app/src/main/java/com/aliif/lpmuas/activty/MainActivity.java
@@ -1,16 +1,78 @@
 package com.aliif.lpmuas.activty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.aliif.lpmuas.R;
+import com.aliif.lpmuas.adapter.ReportAdapter;
+import com.aliif.lpmuas.model.Report;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+    ReportAdapter reportAdapter;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    ArrayList<Report> reports;
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.add_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AddActivity.class));
+            }
+        });
+
+        recyclerView = findViewById(R.id.list_report);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        viewReports();
     }
+
+        private void viewReports()
+        {
+            databaseReference.child("Reports").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    reports = new ArrayList<>();
+                    for (DataSnapshot item: snapshot.getChildren())
+                    {
+                        Report report = item.getValue(Report.class);
+                        report.setId(item.getKey());
+                        reports.add(report);
+                    }
+
+                    reportAdapter = new ReportAdapter(reports, MainActivity.this);
+                    recyclerView.setAdapter(reportAdapter);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
 }
---
+++ app/src/main/java/com/aliif/lpmuas/adapter/ReportAdapter.java
@@ -0,0 +1,79 @@
+package com.aliif.lpmuas.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aliif.lpmuas.R;
+import com.aliif.lpmuas.activty.DetailActivity;
+import com.aliif.lpmuas.model.Report;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+public class ReportAdapter extends RecyclerView.Adapter<ReportAdapter.ReportViewHolder> {
+
+    private List<Report> reportList;
+    private Activity activity;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+    public ReportAdapter(List<Report> reportList, Activity activity){
+        this.reportList = reportList;
+        this.activity = activity;
+    }
+
+
+    @NonNull
+    @Override
+    public ReportAdapter.ReportViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item, parent, false);
+
+        return new ReportViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReportAdapter.ReportViewHolder holder, int position) {
+        final Report report = reportList.get(position);
+        holder.title.setText(report.getTitle());
+        holder.content.setText(report.getContent());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity.getApplicationContext(), DetailActivity.class);
+                intent.putExtra("title", report.getTitle());
+                intent.putExtra("content", report.getContent());
+                intent.putExtra("date", report.getDate());
+                intent.putExtra("location", report.getLocation());
+                intent.putExtra("user_id", report.getUser_id());
+                activity.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ReportViewHolder extends RecyclerView.ViewHolder{
+        TextView title, content;
+        CardView cardView;
+
+        public ReportViewHolder(View itemView){
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            content = itemView.findViewById(R.id.content);
+            cardView = itemView.findViewById(R.id.card);
+        }
+    }
+}
