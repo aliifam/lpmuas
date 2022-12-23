a430f2d3e50685247857189b0be59691e820616a
aliifam <aliif.arief.m@gmail.com>
2022-12-23 20:14:40 +0700

    fix: splash screen

---
+++ app/src/main/java/com/aliif/lpmuas/activty/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.aliif.lpmuas.activty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.aliif.lpmuas.R;
+
+import java.util.Objects;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        int delay = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, delay);
+    }
+}
